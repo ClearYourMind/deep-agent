4d1ac670d27b3247363a61f5e07685c0084cea94
PyDevill <delphadyth@gmail.com>
2026-03-23 00:53:03 +0700

    Made an attempt to optimize message history, asking to summarize tool results and truncating and even clearing tool's messages content

--- src/deep_agent/agent.py
+++ src/deep_agent/agent.py
@@ -1,12 +1,14 @@
 from openai import OpenAI, ChatCompletion
 import json
 import os
-import tools
+
+max_content_length_truncate = 1000
 
 
 class ContextPool:
     def __init__(self, init_message):
         self._messages = [init_message]
+        self._important_messages = [init_message]
         self._max_length = 10000
         self.overflow = False
 
@@ -16,29 +18,56 @@ class ContextPool:
             result_length += len(msg["content"])
         return result_length
 
+    def get_messages_count(self):
+        return len(self._messages)
+
     def append(self, message):
         self._messages.append(message)
         if self.get_context_length() > self._max_length:
-            self.compress_context()
+            self.compress_context_hard()
             self.overflow = True
+        else:
+            self.overflow = False
 
-    def compress_context(self):
+    def compress_context_hard(self):
         print("\t !!! Context max length overflow. It should be compressed !!!")
-        pass
+        for msg in self._messages:
+            if msg["role"] == "tool":
+                msg["content"] = ""
+            else:
+                msg["content"] = (msg["content"][:max_content_length_truncate])
+        self.overflow = False
 
     def get_messages(self):
-        return self._messages[:]
+        result = self._messages[:]
+        return result
 
+    def update_important_messages(self, clear_history=False):
+        msg = self._messages[-2]
+        if (msg["role"] == "tool") and (msg["name"] == "complete"):
+            self._important_messages.append(self._messages[-1])
+            if clear_history:
+                self._messages = self._important_messages[:]
+                self.overflow = False
+
+
+import tools
 
 class Agent:
     def __init__(self, base_url):
         print("Initializing Agent...")
         self._client = OpenAI(api_key=os.environ.get('DEEPSEEK_API_KEY'), base_url=base_url)
-        self._messages = ContextPool({"role": "system", "content": "When you are asked for searching the web, you should consider today's date"})
+        self._messages = ContextPool({"role": "system", "content":
+            """
+                You are run inside a python-script as interactive agent.
+                Main intention is to achieve minimal token cost while maintaining full message history context as much as possible
+            """
+        })
         self._last_response: ChatCompletion = None
         self.wait_prompt = True
 
     def model_request(self):
+        print("Messages count:", self._messages.get_messages_count(), " Context length:", self._messages.get_context_length())
         print("Thinking...")
         response = self._client.chat.completions.create(
             model="deepseek-chat",
@@ -57,7 +86,7 @@ class Agent:
         if calls:
             self.wait_prompt = False
 
-        return self._last_response.choices[0].message.content
+        #return self._last_response.choices[0].message.content
 
     def get_messages(self):
         message_list = ''
@@ -67,7 +96,7 @@ class Agent:
 
     def _use_tool(self, tool) -> str:
         func = tool.function
-        print("Using tool '", func.name, "' ...")
+        print("Using tool '" + func.name + "' ...")
         result = tools.tool_functions[func.name](**json.loads(func.arguments))
         return result
 
@@ -85,6 +114,12 @@ class Agent:
                     "content": result
                 })
                 self.model_request()
+            else: 
+                self._messages.update_important_messages(clear_history=True)
+                print("\n\n")
+                print("Message history:\n")
+                print(self._messages.get_messages())
+                print("\n\n")
 
         return self._last_response.choices[0].message.content
 
--- src/deep_agent/tools.py
+++ src/deep_agent/tools.py
@@ -2,6 +2,10 @@ import os
 import requests
 import json
 
+max_search_result_length = 3000
+max_page_content_length = 5000
+
+
 def search_web(**kwargs):
     print("called 'search_web' tool!")
     print("argument passed: 'query' = " + kwargs["query"])
@@ -17,30 +21,26 @@ def search_web(**kwargs):
 
     response = requests.request("POST", url, headers=headers, json=payload)
     result = json.loads(response.text)
-    print("search tool result:\n\n" + str(result["organic"]) + "\n")
-    return str(result["organic"])
+    print("search tool result:\n\n" + str(result["organic"][:max_search_result_length]) + "\n")
+    return str(result["organic"][:max_search_result_length])
 
 
 def complete(**kwargs):
+
     print("called 'complete' tool!")
     print("argument passed: 'final_thought' = " + kwargs.get("final_thought", "--not used--"))
-    # should ask for next prompt
-    print("Model's final thought: ", kwargs.get("final_thought", "--not used--"))
     return kwargs.get("final_thought", "")
     
     
 def think(**kwargs):
     print("called 'think' tool!")
     print("argument passed: 'analysis' = " + kwargs["analysis"])
-    # just reasoning step
-    print(" >> " + kwargs["analysis"])
     return kwargs["analysis"]
 	
 	
 def ask_clarification(**kwargs):
     print("called 'ask_clarification' tool!")
     print("argument passed: 'question' = " + kwargs["question"])
-    # print(" >> " + kwargs["question'])
     answer = input()
     return answer
 
@@ -62,8 +62,13 @@ def browse_url(**kwargs):
 
     response = requests.request("POST", url, headers=headers, json=payload)
     result = json.loads(response.text)
-    print("Browse tool result:\n\n" + result["text"] + "\n")
-    return result["text"]
+    if "text" in result:
+        if len(result["text"]) > max_page_content_length:
+            result["text"] = (result["text"][:max_page_content_length] + "... <truncated>")
+        print("Browse tool result:\n\n" + result["text"] + "\n")
+        return result["text"]
+    # else:
+    return "Failed to get web page content. Please try another URL"
 
 
 tool_list = [
@@ -71,7 +76,14 @@ tool_list = [
         "type": "function",
         "function": {
             "name": "search_web",
-            "description": "Performs Google search to find most actual information.",
+            "description": """
+                Performs Google search to find most actual information.
+                Along with thought about your next step please specify following information:
+                - summary of search results containing only relevant information for current task.
+                - one or two urls if you are going to visit some links from search results.
+                That information will remain in message history while other search results will be erased.
+                NOTE: snippets of multiple search results may already contain desired info, so actual visiting links is not nessesary in most cases
+            """,
             "parameters": {
                 "type": "object",
                 "properties": {
@@ -88,7 +100,13 @@ tool_list = [
         "type": "function",
         "function": {
             "name": "browse_url",
-            "description": "View content of web-page at specified URL converted to minimalist markdown format",
+            "description": """
+                View content of web-page at specified URL converted to minimalist markdown format. It can be truncated if contents exceed 5000 characters.
+                Along with thought about your next step please specify following information:
+                - summary of content that you are provided containing only information that is relevant to current task in one or two sentences.
+                That summary will persist in message history, while all web-page content will be erased.
+                NOTE: It doesn't return full HTML content.
+            """,
             "parameters": {
                 "type": "object",
                 "properties": {
@@ -105,15 +123,32 @@ tool_list = [
         "type": "function",
         "function": {
             "name": "complete",
-            "description": "FINAL action - call this ONLY when you are ready to provide the final answer. Please do not invoke any other function call after calling this action, because no other action is neeeded.",
+            "description": """
+                FINAL action - call this ONLY when you are ready to provide the final answer.
+                Please summarize the work that has been done during completing task, what actions has been taken and the final result in short.
+                **Example compression:**
+
+                ```
+                The user requested the current date and year.
+
+                Actions performed:
+                1. Search "current date and year today" - 10 results found, key information: March 22, 2026
+                2. Visit https://calendar.yoip.ru/today.html - confirmation: Sunday, March 22, 2026
+
+                Result: today is March 22, 2026 (Sunday)
+                ```
+                That summary will persist in message history while all previous messages are likely to be removed to keep message history as short as possible.
+                Please do not invoke any other function call after calling this action, because no other action is neeeded.
+            """,
             "parameters": {
                 "type": "object",
                 "properties": {
                     "final_thought": {
                         "type": "string",
-                        "description": "Just dummy parameter. Do not use"
+                        "description": "Result of a completed task as summary of performed actions"
                     },
                 },
+                "required": ["final_thought"]
             },
         },
     },
